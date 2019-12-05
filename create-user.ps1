ca79efaeac4eb2913cfd2d0f17aa0fae033f3142
Nikhil Mone <nikhilmone7@gmail.com>
2019-12-05 23:12:18 +0530

    Update create-user.ps1

--- create-user.ps1
+++ create-user.ps1
@@ -1,4 +1,16 @@
-For ($i=1; $i -lt 5; $i++) {Write-Host $i; $Password = [system.web.security.membership]::GeneratePassword(16,4); $User = "welcome"+$i ; Write-Host $Password; Write-Host $User; New-LocalUser $User -Password $Password -FullName "Developer"+$i -Description "This is a developer account"}
+<# Author: Nikhil Mone
+   email: nikhilmone7@gmail.com
+   Disclaimer: Please use at your own risk ! #>
 
+For ($i=1; $i -lt 5; $i++) {
+  Write-Host $i; 
+  $Password = [system.web.security.membership]::GeneratePassword(16,4); 
+  $User = "welcome"+$i ; 
+  <#Write-Host $Password; #>
+  <#Write-Host $User; #>
+  New-LocalUser $User -Password $Password -FullName "Developer"+$i -Description "This is a developer account"
+  }
+
+<# To delete the create users #>
 
 <#For ($i=1; $i -lt 5; $i++) {Remove-LocalUser -Name "welcome"+$i}#>
