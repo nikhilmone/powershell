409e10893e78439af22c9ae158866347722774cf
Nikhil Mone <nikhilmone7@gmail.com>
2019-12-06 14:35:35 +0530

    Add System.Web dependency

--- create-user.ps1
+++ create-user.ps1
@@ -1,7 +1,7 @@
 <# Author: Nikhil Mone
    email: nikhilmone7@gmail.com
    Disclaimer: Please use at your own risk ! #>
-
+Add-Type -AssemblyName System.web;
 For ($i=1; $i -lt 11; $i++) {
   Write-Host $i; 
   $Password = [system.web.security.membership]::GeneratePassword(16,4); 
@@ -17,4 +17,3 @@ For ($i=1; $i -lt 11; $i++) {
 <# To delete the create users #>
 
 <#For ($i=1; $i -lt 11;$i++){$uname = "JBOR"+$i; Remove-LocalUser -Name $uname}#>
-
