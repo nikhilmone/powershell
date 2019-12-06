fd61ec15ec1eb1c1bf48b1b313f19b20d117f3b8
Nikhil Mone <nikhilmone7@gmail.com>
2019-12-06 11:33:34 +0530

    Update create-user.ps1

--- create-user.ps1
+++ create-user.ps1
@@ -2,11 +2,11 @@
    email: nikhilmone7@gmail.com
    Disclaimer: Please use at your own risk ! #>
 
-For ($i=1; $i -lt 5; $i++) {
+For ($i=1; $i -lt 11; $i++) {
   Write-Host $i; 
   $Password = [system.web.security.membership]::GeneratePassword(16,4); 
   $User = "welcome"+$i ; 
-  <#Write-Host $Password; #>
+  Write-Host $Password;
   <#Write-Host $User; #>
   New-LocalUser $User -Password $Password -FullName "Developer"+$i -Description "This is a developer account"
   }
