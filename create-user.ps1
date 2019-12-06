0efaf0d9d59dfa06f63fe3f9c2660e437b168a0c
Nikhil Mone <nikhilmone7@gmail.com>
2019-12-06 12:16:40 +0530

    Update create-user.ps1

--- create-user.ps1
+++ create-user.ps1
@@ -5,10 +5,12 @@
 For ($i=1; $i -lt 11; $i++) {
   Write-Host $i; 
   $Password = [system.web.security.membership]::GeneratePassword(16,4); 
-  $User = "welcome"+$i ; 
   Write-Host $Password;
+  $SecurePassword = ConverTo-SecureString $Password -AsPlainText -Force;
+  $User = "welcome"+$i ; 
+  
   <#Write-Host $User; #>
-  New-LocalUser $User -Password $Password -FullName "Developer"+$i -Description "This is a developer account"
+  New-LocalUser $User -Password $SecurePassword -FullName "Developer"+$i -Description "This is a developer account"
   }
 
 <# To delete the create users #>
