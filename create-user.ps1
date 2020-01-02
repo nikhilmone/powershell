ca5655399bbd54d6cbb1c1521077e9d1ebe32886
Nikhil Mone <nikhilmone7@gmail.com>
2020-01-02 19:10:16 +0530

    Update create-user.ps1

--- create-user.ps1
+++ create-user.ps1
@@ -1,6 +1,13 @@
 <# Author: Nikhil Mone
    email: nikhilmone7@gmail.com
    Disclaimer: Please use at your own risk ! #>
+   
+<# Requirement:
+
+   To create 10 users as JBOR1, JBOR2 .... JBOR10 on a Windows Machine, with random and unique passwords.
+   
+#>
+
 Add-Type -AssemblyName System.web;
 For ($i=1; $i -lt 11; $i++) {
   Write-Host $i; 
@@ -14,6 +21,6 @@ For ($i=1; $i -lt 11; $i++) {
   New-LocalUser $User -Password $SecurePassword -FullName $name -Description "This is a backend developer account"
   }
 
-<# To delete the create users #>
+<# To delete the created users uncomment below line #>
 
 <#For ($i=1; $i -lt 11;$i++){$uname = "JBOR"+$i; Remove-LocalUser -Name $uname}#>
